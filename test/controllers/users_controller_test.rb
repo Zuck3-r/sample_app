62adc46a18fa3429c6ddd95ba42b41671a7f8bef
Ubuntu <ns1122rx@gmail.com>
2021-01-05 14:08:20 +0000

    mail account activation

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -24,10 +24,10 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     log_in_as(@other_user)
     assert_not @other_user.admin?
     patch user_path(@other_user), params: {
-                                    user: { password:              FILL_IN,
-                                            password_confirmation: FILL_IN,
-                                            admin: FILL_IN } }
-    assert_not @other_user.FILL_IN.admin?
+                                    user: { password:              'abc123',
+                                            password_confirmation: 'abc123',
+                                            admin: true } }
+    assert_not @other_user.admin?
   end
   
   test "should redirect index when not logged in" do
